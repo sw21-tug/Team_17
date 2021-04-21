c5bca78d5c84c8f32f5d8c759d3c613d917d2d71
Lukas Preitler <lukas.preitler@student.tugraz.at>
2021-04-21 09:16:42 +0200

    LS-001-A okButton and cancelButton switch to MainActivity [KC, PL]

--- app/src/main/java/com/example/loginsesame/CreateStartUp.kt
+++ app/src/main/java/com/example/loginsesame/CreateStartUp.kt
@@ -3,12 +3,28 @@ package com.example.loginsesame
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
 
 
 class CreateStartUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startup)
+
+        val okButton = findViewById<Button>(R.id.okButton)
+        okButton.setOnClickListener {
+            // TODO Account safe to data base
+            val intentMain = Intent(this@CreateStartUp, MainActivity::class.java)
+            startActivity(intentMain)
+        }
+
+        val cancelButton = findViewById<Button>(R.id.cancelButton)
+        cancelButton.setOnClickListener {
+            val intentMain = Intent(this@CreateStartUp, MainActivity::class.java)
+            startActivity(intentMain)
+        }
+
     }
 
 }
