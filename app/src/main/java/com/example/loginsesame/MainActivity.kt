809aa1c57dac10ebed287c41051901d5f4860ad1
Christina Sophie Knes <christina.knes@student.tugraz.at>
2021-05-01 20:03:19 +0200

    [LS-027] Choose between russian and english implemented [KC, RT]

--- app/src/main/java/com/example/loginsesame/MainActivity.kt
+++ app/src/main/java/com/example/loginsesame/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.loginsesame
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.example.loginsesame.data.User
@@ -11,6 +17,7 @@ import com.example.loginsesame.data.UserDatabase
 import com.example.loginsesame.helper.LogTag
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,12 +30,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadLocale()
         setContentView(R.layout.activity_main)
         isLoggedIn = intent.getBooleanExtra("isLoggedIn", false)
 
         db = UserDatabase.initDb(this)
         userDao = db.getUserDao()
-
 
         var userExists = false
 
@@ -42,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         //no -> open create activity
 
         Log.d(logTag.LOG_MAIN, "is Logged in = " + isLoggedIn)
+        Log.d(logTag.LOG_MAIN, userDao.getAllUsers().size.toString())
         if(!isLoggedIn && userExists){
             openLoginActivity()
         }
@@ -63,5 +71,73 @@ class MainActivity : AppCompatActivity() {
     private fun openLoginActivity() {
         val i = Intent(this@MainActivity, LoginActivity::class.java)
         this@MainActivity.startActivity(i)
+    }
+
+    //remove back button ability
+    override fun onBackPressed() {
+        Log.d(logTag.LOG_LOGIN, "Back-Button Pressed With No Action")
+    }
+
+    //create menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //create menu buttons
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val switchLanguage = item.itemId
+
+        if (switchLanguage == R.id.changeLanguage){
+            //Toast.makeText(this@MainActivity, "New Language", Toast.LENGTH_SHORT).show()
+            showChangeLanguageDialog()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showChangeLanguageDialog(){
+        val adb = AlertDialog.Builder(this)
+        val items = arrayOf<CharSequence>(
+                getString(R.string.eng_Lang),
+                getString(R.string.ru_Lang))
+
+        adb.setSingleChoiceItems(items, -1, DialogInterface.OnClickListener { arg0, arg1 ->
+            if(arg1 == 0)
+                setLanguage("en")
+            else if (arg1 == 1)
+                setLanguage("ru")
+            //add more languages if needed
+        })
+        adb.setPositiveButton("OK",  DialogInterface.OnClickListener { arg0, arg1 ->
+            //refresh application screen
+            recreate()
+        })
+        adb.setTitle(getString(R.string.language_chooser_title))
+        adb.show()
+
+
+    }
+
+    private fun setLanguage(newLang: String){
+        val locale = Locale(newLang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", newLang)
+        editor.apply()
+    }
+
+    private fun loadLocale() {
+        val sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        val language = sharedPreferences.getString("My_Lang", "en")
+        if (language != null) {
+            setLanguage(language)
+        }
+
     }
 }
