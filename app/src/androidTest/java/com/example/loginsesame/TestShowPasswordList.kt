eabbca2250c6f46a94e59a0770bb5b890d79d66a
Lukas Preitler <lukas.preitler@gmail.com>
2021-05-12 11:32:58 +0200

    [LS-002] add TC checkDatabaseAndView [PL, PT]

--- app/src/androidTest/java/com/example/loginsesame/TestShowPasswordList.kt
+++ app/src/androidTest/java/com/example/loginsesame/TestShowPasswordList.kt
@@ -2,6 +2,7 @@ package com.example.loginsesame
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
 import androidx.test.rule.ActivityTestRule
 import com.example.loginsesame.data.UserDao
 import com.example.loginsesame.data.UserDatabase
@@ -38,6 +39,42 @@ class TestShowPasswordList {
     fun closeDb() {
         //vaultEntryDao.deleteAllEntrys()
         db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun checkDatabaseAndView() {
+        Espresso.onView(ViewMatchers.withId(R.id.etInputPassword)).perform(ViewActions.typeText("123456789"))
+
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.withId(R.id.btnInputPasswordOK)).perform(ViewActions.click())
+        assert(vaultEntryDao.allEntrys().size == 5)
+        assert(vaultEntryDao.getEntity("account_a") == vaultEntryDao.allEntrys().get(0))
+        assert(vaultEntryDao.getEntity("account_b") == vaultEntryDao.allEntrys().get(1))
+        assert(vaultEntryDao.getEntity("account_c") == vaultEntryDao.allEntrys().get(2))
+        assert(vaultEntryDao.getEntity("account_d") == vaultEntryDao.allEntrys().get(3))
+        assert(vaultEntryDao.getEntity("account_e") == vaultEntryDao.allEntrys().get(4))
+
+        val recyclerView = activityRule.activity.findViewById<RecyclerView>(R.id.rvAccounts)
+
+        recyclerView.adapter?.itemCount
+
+        Thread.sleep(1000)
+        Espresso.onView(ViewMatchers.withId(R.id.rvAccounts))
+            .inRoot(
+                RootMatchers.withDecorView(
+                    Matchers.`is`(
+                        activityRule.activity.window.decorView
+                    )
+                )
+            )
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+
+        Log.d("COUNT: ", recyclerView.adapter?.itemCount.toString())
+
+        assert(recyclerView.adapter?.itemCount == 5)
     }
 
 }
