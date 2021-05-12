f2b8739d4d0227cad1e6be5f47aee09c202347fa
Lukas Preitler <lukas.preitler@gmail.com>
2021-05-12 11:31:43 +0200

    [LS-002] clean up after testing  [PL, PT]

--- app/src/androidTest/java/com/example/loginsesame/TestShowPasswordList.kt
+++ app/src/androidTest/java/com/example/loginsesame/TestShowPasswordList.kt
@@ -6,9 +6,11 @@ import androidx.test.rule.ActivityTestRule
 import com.example.loginsesame.data.UserDao
 import com.example.loginsesame.data.UserDatabase
 import com.example.loginsesame.data.VaultEntryDao
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
+import java.io.IOException
 
 
 @RunWith(AndroidJUnit4::class)
@@ -28,6 +30,14 @@ class TestShowPasswordList {
         db = UserDatabase.initDb(context)
         userDao = db.getUserDao()
         vaultEntryDao = db.getVaultEntryDao()
+    }
+
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        //vaultEntryDao.deleteAllEntrys()
+        db.close()
     }
 
 }
