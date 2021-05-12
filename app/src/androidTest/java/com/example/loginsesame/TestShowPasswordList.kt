3d1e23392b6e68ba027b78bb738a6ce45f4bd72f
TPichler15 <tobias.pichler99@gmail.com>
2021-05-12 11:59:20 +0200

    [LS-002] start implementation [PL, PT]

--- app/src/androidTest/java/com/example/loginsesame/TestShowPasswordList.kt
+++ app/src/androidTest/java/com/example/loginsesame/TestShowPasswordList.kt
@@ -1,17 +1,28 @@
 package com.example.loginsesame
 
 import android.content.Context
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.example.loginsesame.data.UserDao
 import com.example.loginsesame.data.UserDatabase
 import com.example.loginsesame.data.VaultEntryDao
+import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
+import kotlin.jvm.Throws
 
 
 @RunWith(AndroidJUnit4::class)
