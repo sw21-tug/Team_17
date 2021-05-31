d680687c55528fd8c311ac7e1cd20f20c4c52f92
Bernhard P. LIPPE <bernhard.lippe@student.tugraz.at>
2021-05-31 16:24:36 +0200

    [LS-004] Implement Cancel Function [LB,RT]

--- app/src/main/java/com/example/loginsesame/EditVaultEntry.kt
+++ app/src/main/java/com/example/loginsesame/EditVaultEntry.kt
@@ -6,26 +6,26 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.example.loginsesame.data.User
 import com.example.loginsesame.data.UserDatabase
-import com.example.loginsesame.data.UserRepository
 import com.example.loginsesame.data.VaultEntry
 import com.example.loginsesame.helper.LogTag
 import kotlinx.coroutines.*
 
 class EditVaultEntry : AppCompatActivity() {
-    val logTag = LogTag()
-    lateinit var account : VaultEntry
+    private val logTag = LogTag()
+
+    private lateinit var account : VaultEntry
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_vault_entry)
 
+        setTitle(R.string.vault_edit_title)
         val accountId = intent.getIntExtra("accountId", 0)
 
-        Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "Starting EditVaultEntry with ID: " + accountId)
+
+        Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "We are Here $accountId")
 
         val db = UserDatabase.initDb(this)
         val vaultEntryDao = db.getVaultEntryDao()
@@ -41,6 +41,19 @@ class EditVaultEntry : AppCompatActivity() {
         Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "set url: " + account.url)
         Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "set username: " + account.username)
         Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "set password: " + account.Password)
+
+
+        val cancelButton = findViewById<Button>(R.id.btnVaultCancel)
+        cancelButton.setOnClickListener {
+            Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "cancelButton")
+
+            if (checkEntryChanges()) {
+                alertDialogDiscardChanges()
+            }
+            else {
+                openMainActivity()
+            }
+        }
 
         val btnVaultSave = findViewById<Button>(R.id.btnVaultSave)
 
@@ -64,8 +77,47 @@ class EditVaultEntry : AppCompatActivity() {
             } else {
                 alertDialogNoPassword()
             }
-
         }
+    }
+
+    private fun checkEntryChanges(): Boolean {
+        val newEntryname = findViewById<EditText>(R.id.vaultnameEntry).text.toString()
+        val newUrl = findViewById<EditText>(R.id.vaultURL).text.toString()
+        val newUsername = findViewById<EditText>(R.id.vaultUsername).text.toString()
+        val newPassword = findViewById<EditText>(R.id.vaultPassword).text.toString()
+
+        Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "new entryname: $newEntryname")
+        Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "new url: $newUrl")
+        Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "new username: $newUsername")
+        Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "new password: $newPassword")
+
+
+        return if (account.Name != newEntryname || account.url != newUrl || account.username !=
+            newUsername || account.Password != newPassword) {
+            Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "Values changed")
+            true
+        } else {
+            Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "Values didn't change")
+            false
+        }
+    }
+
+    private fun alertDialogDiscardChanges() {
+
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle(R.string.vault_cancel_changes_alert_title)
+        alert.setMessage(R.string.vault_cancel_changes_alert)
+
+        alert.setPositiveButton(R.string.response_positive) { _, _ ->
+            Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "alertDialogDiscardChanges Answer: Yes")
+
+            openMainActivity()
+        }
+
+        alert.setNegativeButton(R.string.response_negative) { _, _ ->
+            Log.d(logTag.LOG_EDIT_VAULT_ENTRY, "alertDialogDiscardChanges Answer: No")
+        }
+        alert.show()
     }
 
     private fun alertDialogNoPassword() {
@@ -77,10 +129,10 @@ class EditVaultEntry : AppCompatActivity() {
         alert.show()
     }
 
+
     private fun openMainActivity() {
         val i = Intent(this@EditVaultEntry, MainActivity::class.java)
         i.putExtra("isLoggedIn", true)
         this@EditVaultEntry.startActivity(i)
     }
-
 }
